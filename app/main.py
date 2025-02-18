ac78dcee97d684ab60addddee7be3999fe636e7d
Jack Mulligan <mulligja1@gmail.com>
2025-02-18 12:18:13 +0000

    Added CORS declarations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import email
 
 app = FastAPI()
 app.include_router(email.email_router)
+
+# CORS
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
